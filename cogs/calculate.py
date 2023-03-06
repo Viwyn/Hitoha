a2228132cb453172112dfec1448806c1f2b7a1bb
Viwyn <riaruplays@gmail.com>
2023-03-06 09:30:13 +0800

    fixed cal command

--- cogs/calculate.py
+++ cogs/calculate.py
@@ -6,8 +6,8 @@ class Calculate(commands.Cog):
         self.bot = bot
 
     @commands.command(name="calculate", aliases=['cal'], description="Calculate an equation")
-    async def calculate(self, interaction: discord.Interaction, eq):
-        return await interaction.send(eval(eq))
+    async def calculate(self, interaction: discord.Interaction, *eq):
+        return await interaction.send(eval(" ".join(eq)))
     
 async def setup(bot):
     await bot.add_cog(Calculate(bot))
