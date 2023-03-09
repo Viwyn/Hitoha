05b3b7a483d64b6073128df076e263e2b820466e
Viwyn <riaruplays@gmail.com>
2023-03-09 19:33:47 +0800

    made blackjack game

--- cogs/ask.py
+++ cogs/ask.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 import openai
-import json
+from os import remove
 
 class Ask(commands.Cog):
     def __init__(self, bot):
@@ -15,15 +15,27 @@ class Ask(commands.Cog):
         async with interaction.channel.typing():
             response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            max_tokens=2000,
             messages=[
                 {"role": "system", "content": "You are a discord bot called Hitoha that was coded by Riaru that replies to questions asked by users only."},
                 {"role": "user", "content": " ".join(question)}
                 ]    
             )
-    
-        parsed = json.loads(str(response))["choices"][0]["message"]["content"]
-        await interaction.reply(parsed)
+
+            parsed = response["choices"][0]["message"]["content"]
+            length = len(parsed)
+
+            if length > 2000:
+                print(f"Error, response for {interaction.author.display_name} went over the max limit ({length}). \nSending a txt file instead")
+                with open(f"{interaction.author.id}.txt", "a",encoding='utf-8') as file:
+                    file.write(parsed)
+
+                with open(f"{interaction.author.id}.txt", "rb") as file:
+                    await interaction.send(content=f"Sorry,the response went over the max character limit. \nSending a txt file instead", file=discord.File(file, filename="response.txt"))
+
+                remove(f"{interaction.author.id}.txt")
+
+            else:
+                await interaction.reply(parsed)
 
 async def setup(bot):
     await bot.add_cog(Ask(bot))
--- cogs/games.py
+++ cogs/games.py
@@ -1,26 +1,212 @@
 import discord
 from discord.ext import commands
+from random import randrange
+import asyncio
+import mysql.connector
+from os import getenv
 
 class Cards():
     def __init__(self, suit, char):
         self.suit = suit
         self.char = char
 
-        if type(char) == int:
-            value = char
-        elif char in ['j', 'q', 'k']
+    def show(self):
+        detail = ""
+        if self.char in ['j', 'q', 'k', 'a']:
+            detail += self.char.upper()
+        elif type(self.char) == int:
+            detail += str(self.char)
 
+        if self.suit == 'Spades':
+            detail += '♠️'
+        elif self.suit == 'Hearts':
+            detail += '♥️'
+        elif self.suit == 'Clubs':
+            detail += '♣️'
+        elif self.suit == 'Diamonds':
+            detail += '♦️'
+
+        return detail
+
+class Deck():
+    def __init__(self):
+        self.deck = []
+
+        for i in ['Spades', 'Hearts', 'Clubs', 'Diamonds']:
+            for j in [2, 3, 4, 5, 6, 7, 8, 9, 10, 'j', 'q', 'k', 'a']:
+                self.deck.append(Cards(i, j))
+
+    def draw(self):
+        return self.deck.pop(randrange(0, len(self.deck)))
+
+class Hand():
+    def __init__(self):
+        self.hand = []
         self.value = 0
+        self.aces = 0
 
-    
+    def addCard(self, card: Cards):
+        self.hand.append(card)
+        if card.char == 'a':
+            self.value += 11
+            self.aces += 1
+        elif card.char in ['k', 'q', 'j']:
+            self.value += 10
+        elif type(card.char) == int:
+            self.value += card.char
+
+        self.checkAces()
+
+    def checkAces(self):
+        if self.value > 21 and self.aces:
+            self.value -= 10
+            self.aces -= 1
+
+    def hit(self, deck: Deck):
+        self.addCard(deck.draw())
+        self.checkAces()
 
 class Games(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="blackjack", aliases=['blackjack'], help="Start a game of blackjack")
-    async def blackjack(self, interaction: discord.Interaction):
-        pass
+    @commands.command(name="blackjack", aliases=['bj'], help="Start a game of blackjack")
+    async def blackjack(self, interaction: discord.Interaction, bet: int):
+        if bet < 1:
+            return await interaction.reply("Unable to bet nothing")
+
+        connection = mysql.connector.connect(host=getenv("DBHOST"),
+                                                port=getenv("DBPORT"),
+                                                database=getenv("DBNAME"),
+                                                user=getenv("DBUID"),
+                                                password=getenv("DBPW"))
+        cursor = connection.cursor()
+
+        cursor.execute(f"SELECT balance FROM Users WHERE id = {interaction.author.id}")
+        bal = cursor.fetchone()
+
+        if bal == None:
+            return await interaction.reply("You have not made an account yet, run bal to create your account")
+        
+        if bal[0] < bet:
+            return await interaction.reply("You have insufficent balance")
+
+        d1: Deck = Deck()
+        player: Hand = Hand()
+        dealer: Hand = Hand()
+
+        dealer.hit(d1)
+        dealer.hit(d1)
+
+        player.hit(d1)
+        player.hit(d1)
+        
+        stood = False
+        state = None
+
+        message = await interaction.send("Starting a game of blackjack...")
+
+        def check(m):
+            return m.author == interaction.author and m.channel == interaction.channel and m.content.lower().strip() in ['hit', 'stand', 'h', 's']
+
+        while True:
+            msg = "```\n"
+            if stood == False:
+                msg += "Dealer:\n" + " ".join(list("??" for x in dealer.hand)) + f" (??)\n\n" 
+            else:
+                msg += "Dealer:\n" + " ".join(list(x.show() for x in dealer.hand)) + f" ({dealer.value})\n\n" 
+                
+            msg += f"{interaction.author.display_name}:\n" + " ".join(list(x.show() for x in player.hand)) + f" ({player.value})"
+
+            msg += "\n```"
+            await message.edit(content=msg)
+
+            if stood == False:
+                try:
+                    response = await self.bot.wait_for('message', timeout=60.0, check=check)
+
+                    if response.content.lower().strip() in ['hit', 'h']:
+                        player.hit(d1)
+                        if player.value >= 21:
+                            stood = True
+
+                        if len(player.hand) == 5:
+                            stood = True
+
+                        continue
+                    else:
+                        stood = True
+
+                except asyncio.TimeoutError:
+                    await interaction.send("*Game forfeited due to inactivity*")
+                    break
+            else:
+                if player.value > 21 and len(dealer.hand) > 2:
+                    pass
+                elif dealer.value > 21:
+                    pass
+                elif len(dealer.hand) == 5:
+                    pass
+                elif dealer.value < 21 and dealer.value <= player.value:
+                    dealer.hit(d1)
+                    continue
+                else:
+                    dealer.hit(d1)
+                    continue
+
+            #check wins
+            if len(player.hand) == 5:
+                state = 'player'
+                break
+            elif len(dealer.hand) == 5:
+                state = 'dealer'
+                break
+            elif dealer.value == 21 and player.value == 21:
+                state = 'tie'
+                break
+            elif dealer.value == 21 and player.value != 21:
+                state = 'dealer'
+                break
+            elif player.value == 21 and dealer.value != 21:
+                state = 'player'
+                break
+            elif dealer.value < 21 and player.value < 21 and dealer.value > player.value:
+                state = 'dealer'
+                break
+            elif dealer.value < 21 and player.value < 21 and dealer.value < player.value:
+                state = 'player'
+                break
+            elif dealer.value < 21 and player.value > 21:
+                state='dealer'
+                break
+            elif dealer.value > 21 and player.value < 21:
+                state='player'
+                break
+            else:
+                pass
+
+        msg = "```\n"
+        msg += "Dealer:\n" + " ".join(list(x.show() for x in dealer.hand)) + f" ({dealer.value})\n\n" 
+        msg += f"{interaction.author.display_name}:\n" + " ".join(list(x.show() for x in player.hand)) + f" ({player.value})"
+        msg += "\n```"
+
+        if state == 'player':
+            msg += f'\n{interaction.author.display_name} Wins!'
+            msg += f'\n+{bet}'
+            cursor.execute(f"UPDATE Users SET balance = {bal[0]+bet} WHERE id = {interaction.author.id}")
+        elif state == 'dealer':
+            msg += f'\nHitoha Wins!'
+            msg += f'\n-{bet}'
+            cursor.execute(f"UPDATE Users SET balance = {bal[0]-bet} WHERE id = {interaction.author.id}")
+        elif state == 'tie':
+            msg += "\nGame ended in a tie!"
+
+        
+        await message.edit(content=msg)
+        connection.commit()
+        cursor.close()
+        connection.close()
+
 
 async def setup(bot):
     await bot.add_cog(Games(bot))
